4ec8beb90223fe31ddbbd8ad9a814dc4a210c3b9
Adam Zielonka <root@adamzielonka.pro>
2020-04-04 14:18:07 +0200

    Update docker

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,6 +7,16 @@ rm -fr build
 cp -r src build
 dosbox -c "MOUNT C: $PWD" -c "MOUNT D: /tp" -c "c:" -c "CD C:\BUILD" -c "D:\BIN\TPC.EXE $1 > LOG.TXT" -c "EXIT"
 cat build/LOG.TXT
-zip -rj build/DESKA.ZIP build/
-unzip 3rd-party/js-dos.zip -d build/
-cp web/index.html build/index.html
+zip -rj build/BUILD.ZIP build/
+
+MD5_ZIP_SUM=$(md5sum build/BUILD.ZIP | cut -d ' ' -f 1)
+ZIP_FILE_NAME=$1-$MD5_ZIP_SUM.ZIP
+echo $ZIP_FILE_NAME
+mv build/BUILD.ZIP build/$ZIP_FILE_NAME
+
+cp /web/* build/
+
+TITLE=$(echo $1 | cut -d '.' -f 1)
+sed -i "s/!!!TITLE!!!/$TITLE/g" build/index.html
+sed -i "s/!!!ZIP_FILENAME!!!/$ZIP_FILE_NAME/g" build/index.html
+sed -i "s/!!!EXE_FILENAME!!!/$TITLE.EXE/g" build/index.html
--- docker/dosbox.dockerfile
+++ docker/dosbox.dockerfile
@@ -10,6 +10,11 @@ RUN mkdir /tp
 COPY 3rd-party/TPWDB.ZIP TPWDB.ZIP
 RUN unzip TPWDB.ZIP -d /tp
 
+RUN mkdir /web
+COPY 3rd-party/js-dos.zip js-dos.zip
+RUN unzip js-dos.zip -d /web
+COPY docker/index.html /web/index.html
+
 COPY docker/docker-entrypoint.sh docker-entrypoint.sh
 RUN chmod +x ./docker-entrypoint.sh
 
