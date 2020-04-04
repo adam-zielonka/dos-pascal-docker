9c9fbb6f7b446135a4b6e3020bfb77387cc48d8b
Adam Zielonka <root@adamzielonka.pro>
2020-04-04 17:08:01 +0200

    Uptate public zip file name

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,20 +3,20 @@
 set -x
 
 FILE=$1
+TITLE=$(echo $1 | cut -d '.' -f 1)
 rm -fr build
 cp -r src build
 dosbox -c "MOUNT C: $PWD" -c "MOUNT D: /tp" -c "c:" -c "CD C:\BUILD" -c "D:\BIN\TPC.EXE $1 > LOG.TXT" -c "EXIT"
 cat build/LOG.TXT
-zip -rj build/BUILD.ZIP build/
+zip -rj build/$TITLE.ZIP build/
 
 MD5_ZIP_SUM=$(md5sum build/BUILD.ZIP | cut -d ' ' -f 1)
 ZIP_FILE_NAME=$1-$MD5_ZIP_SUM.ZIP
 echo $ZIP_FILE_NAME
-mv build/BUILD.ZIP build/$ZIP_FILE_NAME
+cp build/$TITLE.ZIP build/$ZIP_FILE_NAME
 
 cp /web/* build/
 
-TITLE=$(echo $1 | cut -d '.' -f 1)
 sed -i "s/!!!TITLE!!!/$TITLE/g" build/index.html
 sed -i "s/!!!ZIP_FILENAME!!!/$ZIP_FILE_NAME/g" build/index.html
 sed -i "s/!!!EXE_FILENAME!!!/$TITLE.EXE/g" build/index.html
