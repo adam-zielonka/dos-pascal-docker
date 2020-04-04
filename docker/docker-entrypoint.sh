e53888bc99c820b8cd10c1bf9fd466935c5d5d21
Adam Zielonka <root@adamzielonka.pro>
2020-04-04 11:49:55 +0200

    Update docker conf and ci

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+set -x
+
+FILE=$1
 rm -fr build
 cp -r src build
-dosbox
+dosbox -c "MOUNT C: $PWD" -c "MOUNT D: /tp" -c "c:" -c "CD C:\BUILD" -c "D:\BIN\TPC.EXE $1 > LOG.TXT" -c "EXIT"
 cat build/LOG.TXT
 zip -rj build/DESKA.ZIP build/
 unzip 3rd-party/js-dos.zip -d build/
