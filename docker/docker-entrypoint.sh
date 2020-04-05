8983bd31490a8e6019a0d15ec107f6c72e413e40
Adam Zielonka <root@adamzielonka.pro>
2020-04-05 10:56:38 +0200

    Add exit when compile was not succesful

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,6 +8,7 @@ rm -fr build
 cp -r src build
 dosbox -c "MOUNT C: $PWD" -c "MOUNT D: /tp" -c "c:" -c "CD C:\BUILD" -c "D:\BIN\TPC.EXE $1 > LOG.TXT" -c "EXIT"
 cat build/LOG.TXT
+if cat build/LOG.TXT | grep -q Error ; then exit 500 ; fi
 zip -rj build/$TITLE.ZIP build/
 
 MD5_ZIP_SUM=$(md5sum build/$TITLE.ZIP | cut -d ' ' -f 1)
