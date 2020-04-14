7f4e31db4c898ab1880d5099a39fa6f54a0a4289
Adam Zielonka <root@adamzielonka.pro>
2020-04-14 11:12:15 +0200

    Add missing arg to xargs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,8 +20,8 @@ if [[ "$2" == "--watch" ]] ; then
       rm -fr build
       mkdir build
       cp -r `ls -Ad $DIR/* | grep -v "build"` build
-      ls -1Ad build/*.[Pp][Aa][Ss] | xargs encode.sh
-      ls -1Ad build/*.[Tt][Xx][Tt] | xargs encode.sh
+      ls -1Ad build/*.[Pp][Aa][Ss] | xargs -n 1 encode.sh
+      ls -1Ad build/*.[Tt][Xx][Tt] | xargs -n 1 encode.sh
       dosbox -c "MOUNT C: $PWD" -c "MOUNT D: /tpc" -c "c:" -c "CD C:\BUILD" -c "D:\TPC.EXE $FILE -B > LOG.TXT" -c "EXIT" > /dev/null
       decode.sh build/LOG.TXT
       cat build/LOG.TXT
@@ -34,8 +34,8 @@ fi
 rm -fr build
 mkdir build
 cp -r `ls -Ad $DIR/* | grep -v "build"` build
-ls -1Ad build/*.[Pp][Aa][Ss] | xargs encode.sh 
-ls -1Ad build/*.[Tt][Xx][Tt] | xargs encode.sh
+ls -1Ad build/*.[Pp][Aa][Ss] | xargs -n 1 encode.sh
+ls -1Ad build/*.[Tt][Xx][Tt] | xargs -n 1 encode.sh
 dosbox -c "MOUNT C: $PWD" -c "MOUNT D: /tpc" -c "c:" -c "CD C:\BUILD" -c "D:\TPC.EXE $FILE -B > LOG.TXT" -c "EXIT" > /dev/null
 decode.sh build/LOG.TXT
 cat build/LOG.TXT
