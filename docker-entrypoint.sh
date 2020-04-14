bc8e6875e722bb3f75c5e7b36411aadefb20a20e
Adam Zielonka <root@adamzielonka.pro>
2020-04-14 10:29:38 +0200

    Add build all units flag to tpc

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -36,7 +36,7 @@ mkdir build
 cp -r `ls -Ad $DIR/* | grep -v "build"` build
 ls -1Ad build/*.[Pp][Aa][Ss] | xargs encode.sh 
 ls -1Ad build/*.[Tt][Xx][Tt] | xargs encode.sh
-dosbox -c "MOUNT C: $PWD" -c "MOUNT D: /tpc" -c "c:" -c "CD C:\BUILD" -c "D:\TPC.EXE $FILE > LOG.TXT" -c "EXIT" > /dev/null
+dosbox -c "MOUNT C: $PWD" -c "MOUNT D: /tpc" -c "c:" -c "CD C:\BUILD" -c "D:\TPC.EXE $FILE -B > LOG.TXT" -c "EXIT" > /dev/null
 decode.sh build/LOG.TXT
 cat build/LOG.TXT
 if cat build/LOG.TXT | grep -q Error ; then exit 1 ; fi
